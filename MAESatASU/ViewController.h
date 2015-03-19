6270c8435618d6099a63e923f654c7076b995304
Leonardo Andrade Osorio <Leo@leonardos-mbp.mobile.asu.edu>
2015-03-18 17:59:56 -0700

    Initial Commit

---
+++ MAESatASU/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MAESatASU
+//
+//  Created by Leonardo Andrade Osorio on 3/18/15.
+//  Copyright (c) 2015 Leonardo Andrade Osorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MAESatASU/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MAESatASU
+//
+//  Created by Leonardo Andrade Osorio on 3/18/15.
+//  Copyright (c) 2015 Leonardo Andrade Osorio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
