762a4fcc731448639f46b858301bc2c988f4a8d3
leo-andrade <laandra1@asu.edu>
2015-04-03 01:31:16 -0700

    second commit

---
+++ MAESatASU/EventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventsViewController.h
+//  MAESatASU
+//
+//  Created by Leonardo Andrade Osorio on 4/2/15.
+//  Copyright (c) 2015 Leonardo Andrade Osorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsViewController : UIViewController
+
+@end
--- MAESatASU/EventsViewController.m
+++ MAESatASU/EventsViewController.m
@@ -1,18 +1,18 @@
 //
-//  splashPageViewController.m
+//  EventsViewController.m
 //  MAESatASU
 //
-//  Created by Leonardo Andrade Osorio on 3/19/15.
+//  Created by Leonardo Andrade Osorio on 4/2/15.
 //  Copyright (c) 2015 Leonardo Andrade Osorio. All rights reserved.
 //
 
-#import "splashPageViewController.h"
+#import "EventsViewController.h"
 
-@interface splashPageViewController ()
+@interface EventsViewController ()
 
 @end
 
-@implementation splashPageViewController
+@implementation EventsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
